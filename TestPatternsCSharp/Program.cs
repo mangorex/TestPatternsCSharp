993ea04c20c8f265b289f229372d66c11ed20f7a
mangorex <mangorex89@gmail.com>
2022-03-14 19:24:22 +0100

    Basic implementation of Singleton Non Thread Safe

---
+++ TestPatternsCSharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TestPatternsCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing Singleton Pattern!");
+
+            static void Main(string[] args)
+            {
+                // The client code.
+                SingletonNonThreadSafe s1 = SingletonNonThreadSafe.GetInstance();
+                SingletonNonThreadSafe s2 = SingletonNonThreadSafe.GetInstance();
+
+                if (s1 == s2)
+                {
+                    Console.WriteLine("Singleton works, both variables contain the same instance.");
+                }
+                else
+                {
+                    Console.WriteLine("Singleton failed, variables contain different instances.");
+                }
+            }
+        }
+    }
+}
